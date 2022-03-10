ab97d6b7ad4f013fc70e2d08e46acab691dd1459
Vahid <VJalili@users.noreply.github.com>
2022-03-10 16:07:23 -0500

    Refactor edge serialization & add block height to serialized properties.

--- BC2G/Graph/Edge.cs
+++ BC2G/Graph/Edge.cs
@@ -7,18 +7,53 @@
         public double Value { get; }
         public EdgeType Type { get; }
         public uint Timestamp { get; }
+        public int BlockHeight { get; }
+
+        public static string Header
+        {
+            get
+            {
+                return string.Join(_delimiter, new string[]
+                {
+                    "Source",
+                    "Target",
+                    "Weight",
+                    "EdgeType",
+                    "Timestamp",
+                    "TimeOffsetFromGenesisBlock",
+                    "BlockHeight"
+                });
+            }
+        }
+
+        private const string _delimiter = ",";
 
 
         public Edge(
             string source, string target,
             double value, EdgeType type,
-            uint timestamp)
+            uint timestamp, int blockHeight)
         {
             Source = source;
             Target = target;
             Value = value;
             Type = type;
             Timestamp = timestamp;
+            BlockHeight = blockHeight;
+        }
+
+        public string ToString(string sourceId, string targetId)
+        {
+            return string.Join(_delimiter, new string[]
+            {
+                sourceId,
+                targetId,
+                Value.ToString(),
+                ((int)Type).ToString(),
+                Timestamp.ToString(),
+                (Timestamp - BitcoinAgent.GenesisTimestamp).ToString(),
+                BlockHeight.ToString()
+            });
         }
 
         public int GetHashCode(bool ignoreValue)
