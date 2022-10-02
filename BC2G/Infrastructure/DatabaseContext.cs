c439466aa0c83b271f49c174eb4e710040cb3975
Vahid <VJalili@users.noreply.github.com>
2022-10-01 22:43:16 -0400

    Define & use methods for optimistic db context save (AddOrUpdate).

--- BC2G/Infrastructure/DatabaseContext.cs
+++ BC2G/Infrastructure/DatabaseContext.cs
@@ -1,5 +1,10 @@
 ﻿using BC2G.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Design;
+using Npgsql;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +13,42 @@ using System.Threading.Tasks;
 
 namespace BC2G.Infrastructure
 {
+/*
+    public class BloggingContextFactory : IDesignTimeDbContextFactory<DatabaseContext>
+    {
+        public DatabaseContext CreateDbContext(string[] args)
+        {
+            Console.WriteLine("I am used.------------------");
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            optionsBuilder.UseNpgsql("Data Source=blog.db");
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
+    }*/
+
+    /*
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        private readonly string _connectionString;
+
+        public DatabaseContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Console.WriteLine("I am called ***********************");
+            optionsBuilder.UseNpgsql("Host=localhost");
+        }
+    }*/
+
+
     public class DatabaseContext : DbContext
     {
         /*
@@ -17,11 +58,18 @@ namespace BC2G.Infrastructure
         private readonly string _password;*/
 
         public DbSet<Utxo> Utxos => Set<Utxo>();
+        //public DbSet<UtxoB> UtxosB;
 
         // TODO: Currently this constructor is needed to add migration scripts, 
         // see how this requirement can be better addressed. 
-        public DatabaseContext() { }
-        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+        /*public DatabaseContext() : base()
+        {
+            Console.WriteLine("I am called 2");
+        }*/
+
+        // Needed by the migration scripts
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
 
         /*
         public DatabaseContext(string host, string database, string username, string password)
@@ -44,14 +92,99 @@ namespace BC2G.Infrastructure
             // https://www.npgsql.org/efcore/modeling/concurrency.html
             builder.Entity<Utxo>().UseXminAsConcurrencyToken();
         }
-        /*
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        public static async Task OptimisticAddOrUpdate(
+            DatabaseContext context,
+            IDbContextFactory<DatabaseContext> contextFactory,
+            CancellationToken ct)
         {
-            optionsBuilder.UseNpgsql(
-                $"Host={_host};" +
-                $"Database={_database};" +
-                $"Username={_username};" +
-                $"Password={_password}");
-        }*/
+            await OptimisticTx(async () =>
+            {
+                await context.SaveChangesAsync(ct);
+            },
+            async () =>
+            {
+                var enties = context.ChangeTracker.Entries<Utxo>();
+                var utxos = enties.Select(x => (Utxo)x.CurrentValues.ToObject());
+                await ResilientAddOrUpdate(utxos, contextFactory, ct);
+            });
+        }
+
+        public static async Task OptimisticAddOrUpdate(
+            ICollection<Utxo> utxos,
+            IDbContextFactory<DatabaseContext> contextFactory,
+            CancellationToken ct)
+        {
+            await OptimisticTx(async () =>
+            {
+                using var c = contextFactory.CreateDbContext();
+                await c.Utxos.AddRangeAsync(utxos, ct);
+                await c.SaveChangesAsync(ct);
+            },
+            async () =>
+            {
+                await ResilientAddOrUpdate(utxos, contextFactory, ct);
+            });
+        }
+
+        private static async Task OptimisticTx(Func<Task> tx, Func<Task> onFailure)
+        {
+            try { await tx(); }
+            catch (Exception e)
+            {
+                switch (e)
+                {
+                    case InvalidOperationException:
+                    case DbUpdateConcurrencyException:
+                    case DbUpdateException when (
+                        e.InnerException is PostgresException pe &&
+                        pe.SqlState == "23505"):
+                        await onFailure();
+                        break;
+
+                    default: throw;
+                }
+            }
+        }
+
+        private static async Task ResilientAddOrUpdate(
+            IEnumerable<Utxo> utxos,
+            IDbContextFactory<DatabaseContext> contextFactory,
+            CancellationToken ct)
+        {
+            foreach (var utxo in utxos)
+            {
+                var policy = Policy
+                    .Handle<InvalidOperationException>()
+                    .Or<DbUpdateConcurrencyException>()
+                    .Or<DbUpdateException>(
+                        e => e.InnerException is PostgresException pe &&
+                        pe.SqlState == "23505")
+                    .WaitAndRetryAsync(Backoff.DecorrelatedJitterBackoffV2(
+                        TimeSpan.FromSeconds(10), 3));
+
+                await policy.ExecuteAsync(async () =>
+                {
+                    using var c = contextFactory.CreateDbContext();
+                    var inDbUtxo = await c.Utxos.FindAsync(utxo.Id);
+                    if (inDbUtxo == null)
+                    {
+                        await c.Utxos.AddAsync(utxo, ct);
+                    }
+                    else
+                    {
+                        inDbUtxo.AddCreatedIn(utxo.CreatedIn);
+                        inDbUtxo.AddReferencedIn(utxo.ReferencedIn);
+                    }
+
+                    await c.SaveChangesAsync(ct);
+                });
+            }
+        }
     }
 }
+
+
+
+
+
