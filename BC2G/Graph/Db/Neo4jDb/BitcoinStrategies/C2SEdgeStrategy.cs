bf43710a7181a008b7eb1dd664181fbf3ed5390f
Vahid <VJalili@users.noreply.github.com>
2024-11-04 22:02:43 -0500

    Syntax refactoring & add a comment.

--- BC2G/Graph/Db/Neo4jDb/BitcoinStrategies/C2SEdgeStrategy.cs
+++ BC2G/Graph/Db/Neo4jDb/BitcoinStrategies/C2SEdgeStrategy.cs
@@ -4,13 +4,13 @@ public class C2SEdgeStrategy : S2SEdgeStrategy
 {
     /// Note that the ordre of the items in this array should 
     /// match those in the `ToCSV` method.
-    private readonly Property[] _properties = new Property[]
-    {
+    private readonly Property[] _properties =
+    [
         Props.EdgeTargetAddress,
         Props.EdgeType,
         Props.EdgeValue,
         Props.Height
-    };
+    ];
 
     public override string GetCsvHeader()
     {
@@ -27,13 +27,13 @@ public class C2SEdgeStrategy : S2SEdgeStrategy
     {
         /// Note that the ordre of the items in this array should 
         /// match those in the `_properties`. 
-        return string.Join(Neo4jDb.csvDelimiter, new string[]
-        {
+        return string.Join(Neo4jDb.csvDelimiter,
+        [
             edge.Target.Address,
             edge.Type.ToString(),
             edge.Value.ToString(),
             edge.BlockHeight.ToString()
-        });
+        ]);
     }
 
     public override string GetQuery(string csvFilename)
