2d7ef0ff74c251ea4f34bef7d78f35c9ab6491a6
Vahid <VJalili@users.noreply.github.com>
2025-09-21 09:48:26 -0400

    Refactor solution & project names.

--- EBA/Properties/Resources.Designer.cs
+++ EBA/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace BC2G.Properties {
+namespace EBA.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace BC2G.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BC2G.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("EBA.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
