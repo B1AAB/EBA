fb128eafdda7ce77ad1e325c7bfab44ddf17d1da
Vahid <VJalili@users.noreply.github.com>
2025-12-28 23:28:32 -0500

    Bug fix creating an instance of coinbase node from db data.

--- EBA/Blockchains/Bitcoin/Graph/CoinbaseNode.cs
+++ EBA/Blockchains/Bitcoin/Graph/CoinbaseNode.cs
@@ -5,7 +5,15 @@ public class CoinbaseNode : Node
     public new static GraphComponentType ComponentType { get { return GraphComponentType.BitcoinCoinbaseNode; } }
     public override GraphComponentType GetGraphComponentType() { return ComponentType; }
 
-    public CoinbaseNode(Neo4j.Driver.INode node, double? originalOutdegree = null, double? hopsFromRoot = null) : base(node.ElementId, originalOutDegree: originalOutdegree, outHopsFromRoot: hopsFromRoot)
+    public CoinbaseNode(
+        Neo4j.Driver.INode node,
+        double? originalOutdegree = null,
+        double? hopsFromRoot = null) : 
+        base("Coinbase",
+            originalInDegree: 0,
+            originalOutDegree: originalOutdegree,
+            outHopsFromRoot: hopsFromRoot,
+            idInGraphDb: node.ElementId)
     { }
 
     public override string GetIdPropertyName()
