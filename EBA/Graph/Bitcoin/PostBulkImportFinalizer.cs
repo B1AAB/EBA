5943e05b3e6f4818ffeea54bfc22e67b781f56c0
Vahid <VJalili@users.noreply.github.com>
2026-03-31 21:26:12 -0400

    Add bulk import finalizer

---
+++ EBA/Graph/Bitcoin/PostBulkImportFinalizer.cs
@@ -0,0 +1,100 @@
+﻿using EBA.Graph.Bitcoin.Factories;
+
+namespace EBA.Graph.Bitcoin;
+
+public class PostBulkImportFinalizer(
+    Options options, 
+    IGraphDb<BitcoinGraph> graphDb,
+    ILogger<BitcoinGraphAgent> logger)
+{
+    private readonly Options _options = options;
+    private readonly IGraphDb<BitcoinGraph> _graphDb = graphDb;
+    private readonly ILogger<BitcoinGraphAgent> _logger = logger;
+
+    public async Task Finalize(CancellationToken ct)
+    {
+        await AddSchemaAndSeeding(ct);
+
+        await SetSupplyAmount(ct);
+
+        await _graphDb.SetUTxOSpentHeight(ct);
+    }
+
+    private async Task AddSchemaAndSeeding(CancellationToken ct)
+    {
+        var schemas = new List<string>();
+        var seedingCommands = new List<string>();
+        foreach (var nodeStrategy in _graphDb.StrategyFactory.NodeStrategies)
+        {
+            schemas.AddRange(nodeStrategy.Value.GetSchemaConfigs());
+            seedingCommands.AddRange(nodeStrategy.Value.GetSeedingCommands());
+        }
+        foreach (var edgeStrategy in _graphDb.StrategyFactory.EdgeStrategies)
+        {
+            schemas.AddRange(edgeStrategy.Value.GetSchemaConfigs());
+            seedingCommands.AddRange(edgeStrategy.Value.GetSeedingCommands());
+        }
+
+        _logger.LogInformation("Executing {count:n0} schema configuration commands.", schemas.Count);
+        await _graphDb.ExecuteWriteQueryAsync(schemas, ct);
+
+        _logger.LogInformation("Executing {count:n0} seeding commands.", seedingCommands.Count);
+        await _graphDb.ExecuteWriteQueryAsync(seedingCommands, ct);
+    }
+
+    private async Task SetSupplyAmount(CancellationToken ct)
+    {
+        _logger.LogInformation("Fetching blocks from graph database.");
+        var nodeVar = "n";
+        var records = await _graphDb.GetNodesAsync(NodeKind.Block, CancellationToken.None, nodeVariable: nodeVar);
+        _logger.LogInformation("Retrieved {count:n0} records from graph database. Creating block nodes.", records.Count);
+
+        NodeFactory.TryCreate<BlockNode>(records, out var blockNodes, nodeVar);
+
+        var blocks = new SortedList<long, BlockNode>();
+        foreach (var block in blockNodes)
+            blocks.Add(block.BlockMetadata.Height, block);        
+
+        if (blocks.First().Value.BlockMetadata.Height != 0)
+        {
+            throw new InvalidOperationException(
+                $"The first block in the graph has height " +
+                $"{blocks.First().Value.BlockMetadata.Height:,}, " +
+                $"expected 0.");
+        }
+
+        if (blocks.Last().Value.BlockMetadata.Height != blocks.Count - 1)
+        {
+            throw new InvalidOperationException(
+                $"This operation requires a continues set of blocks, there are missing blocks");
+        }
+
+        blocks[0].BlockMetadata.TotalSupply = blocks[0].BlockMetadata.MintedBitcoins;
+        blocks[0].BlockMetadata.TotalSupplyNominal = blocks[0].BlockMetadata.TotalSupply;
+        for (var i = 1; i < blocks.Count; i++)
+        {
+            blocks[i].BlockMetadata.TotalSupply =
+                blocks[i - 1].BlockMetadata.TotalSupply +
+                blocks[i].TripletTypeValueSum[C2TEdge.Kind] -
+                blocks[i].BlockMetadata.ProvablyUnspendableBitcoins;
+
+            blocks[i].BlockMetadata.TotalSupplyNominal =
+                blocks[i - 1].BlockMetadata.TotalSupplyNominal + blocks[i].BlockMetadata.MintedBitcoins;
+        }
+
+        var updates = blocks.Values.Select(b => new Dictionary<string, object?>
+        {
+            [nameof(BlockMetadata.Height)] = b.BlockMetadata.Height,
+            [nameof(BlockMetadata.TotalSupply)] = b.BlockMetadata.TotalSupply,
+            [nameof(BlockMetadata.TotalSupplyNominal)] = b.BlockMetadata.TotalSupplyNominal,
+        }).ToList();
+
+        _logger.LogInformation("Pushing {count:n0} block updates to graph database.", updates.Count);
+        await _graphDb.BulkUpdateNodePropertiesAsync(
+            NodeKind.Block,
+            nameof(BlockMetadata.Height),
+            updates,
+            ct);
+        _logger.LogInformation("Completed pushing block updates.");
+    }
+}
