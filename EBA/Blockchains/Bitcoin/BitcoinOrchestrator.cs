59a8892614726fad3f7ecb95c563ab9be0c94842
Vahid <VJalili@users.noreply.github.com>
2026-04-19 19:48:47 -0400

    refactor

--- EBA/Blockchains/Bitcoin/BitcoinOrchestrator.cs
+++ EBA/Blockchains/Bitcoin/BitcoinOrchestrator.cs
@@ -215,7 +215,7 @@ public class BitcoinOrchestrator : IBlockchainOrchestrator
         // TODO: refactor the following so that only options is passed to the buffer
 
         using var gBuffer = new PersistentGraphBuffer(
-            graphAgent: _host.Services.GetRequiredService<Graph.Bitcoin.BitcoinGraphOrchestrator>(),
+            graphOrchestrator: _host.Services.GetRequiredService<Graph.Bitcoin.BitcoinGraphOrchestrator>(),
             logger: _host.Services.GetRequiredService<ILogger<PersistentGraphBuffer>>(),
             pTxoLifeCyccleLogger: options.Bitcoin.Traverse.TrackTxo ?_host.Services.GetRequiredService<ILogger<PersistentTxoLifeCycleBuffer>>() : null,
             txoLifeCycleFilename: options.Bitcoin.Traverse.TrackTxo ? options.Bitcoin.Traverse.TxoFilename : null,
